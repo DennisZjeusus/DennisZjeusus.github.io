041c82ffbaf269249d83f909bceeb6a3925c8ed1
DennisZjeusus <40574195+DennisZjeusus@users.noreply.github.com>
2020-08-12 05:42:04 -0400

    Ananda magra beta two
    second beta of ananda magra foundation platform

--- js/button.js
+++ js/button.js
@@ -1,18 +1,18 @@
-  $('#one').on('click', function () {
-    $( "#gasb" ).trigger( "click" );
-  });
-  $('#agburner').on('click', function () {
-    $( "#gburnerb" ).trigger( "click" );
-  });
-  $('#agcookerb').on('click', function () {
-    $( "#gcookerb" ).trigger( "click" );
-  });
-  $('#agpipeb').on('click', function () {
-    $( "#gpipeb" ).trigger( "click" );
-  });
-  $('#agvalveb').on('click', function () {
-    $( "#gvalveb" ).trigger( "click" );
-  });
-  $('#aggrillb').on('click', function () {
-    $( "#ggrillb" ).trigger( "click" );
-  });
+  // $('#one').on('click', function () {
+  //   $( "#gasb" ).trigger( "click" );
+  // });
+  // $('#agburner').on('click', function () {
+  //   $( "#gburnerb" ).trigger( "click" );
+  // });
+  // $('#agcookerb').on('click', function () {
+  //   $( "#gcookerb" ).trigger( "click" );
+  // });
+  // $('#agpipeb').on('click', function () {
+  //   $( "#gpipeb" ).trigger( "click" );
+  // });
+  // $('#agvalveb').on('click', function () {
+  //   $( "#gvalveb" ).trigger( "click" );
+  // });
+  // $('#aggrillb').on('click', function () {
+  //   $( "#ggrillb" ).trigger( "click" );
+  // });
